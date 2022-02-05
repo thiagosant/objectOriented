f725f9fdc0f71387b0d4fcca43066838b44bc7ef
Thiago S. Melo <thiagosantiagomelo@gmail.com>
2022-02-05 17:56:48 -0300

    Using Private Set to block var changes

--- src/main/kotlin/com/sansoft/digionebank/Person.kt
+++ src/main/kotlin/com/sansoft/digionebank/Person.kt
@@ -4,17 +4,13 @@ class Person {
     var name: String = "Thiago"
     var cpf: String = "123.123.123-11"
 
-    inner class Address {
-        var rua: String = "Address test"
-    }
+    private set
 }
 
 fun main() {
     val thiago = Person()
 
+    println(thiago)
     println(thiago.name)
     println(thiago.cpf)
-
-
-    println(thiago.Address().rua)
 }
