019084149b901aae7388814b2575369e85002d1f
Thiago S. Melo <thiagosantiagomelo@gmail.com>
2022-02-05 18:12:23 -0300

    Created Account Class and implemented the function Deposit and Withdraw

---
+++ src/main/kotlin/com/sansoft/digionebank/Account.kt
@@ -0,0 +1,15 @@
+package com.sansoft.digionebank
+
+import java.math.BigDecimal
+
+class Account (val agency: String, val number: String, val balance: BigDecimal){
+
+    fun deposit(value: BigDecimal){
+
+    }
+
+    fun withdraw(value: BigDecimal){
+
+    }
+
+}
